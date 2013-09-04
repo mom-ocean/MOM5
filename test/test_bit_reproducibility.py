966a79ecfea38fadd33046acada2cc6b55ff2a0e
Nicholas Hannah <nicholas.hannah@unsw.edu.au>
2013-09-04 16:46:12 +1000

    Additions to the bit reproducibility tests.

--- test/test_bit_reproducibility.py
+++ test/test_bit_reproducibility.py
@@ -1,31 +1,62 @@
 
 import os
+import sys
 import subprocess
 import time
+
+# qsub doesn't like the -l wd option on the command line, so need to make a script.
+
+run_script = """
+#!/bin/csh -f
+
+#PBS -P v45
+#PBS -q express
+#PBS -l walltime=00:10:00
+#PBS -l ncpus=32
+#PBS -l mem=63Gb
+#PBS -l wd
+
+./MOM_run.csh --platform nci --type %s --experiment %s --download_input_data
+"""
 
 class ModelTestSetup:
 
     def __init__(self): 
-        pass
+
+        self.exp_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../', 'exp')
 
     def build(self, model_type):
 
-        os.chdir(os.path.join(os.path.realpath(__file__), '../', 'exp'))
-        return subprocess.check_call(['./MOM_compile.csh', '--platform', 'nci', '--type', model_type])
+        os.chdir(self.exp_path)
+        ret = subprocess.check_call(['./MOM_compile.csh', '--platform', 'nci', '--type', model_type])
+
+        if ret == 0:
+            return True
+        else:
+            return False
 
     def run(self, model_type, exp):
 
-        os.chdir(os.path.join(os.path.realpath(__file__), '../', 'exp'))
+        os.chdir(self.exp_path)
         
+        # Write script out as a file.
+        with open('run_script.sh', 'w+') as f:
+            f.write(run_script % (model_type, exp))
+
         # Submit the experiment
-        qsub_args = ['qsub', '-P', 'v45', '-q', 'normal', '-k', 'oe', '-l', 'walltime=00:10:00,ncpus=16,mem=31Gb', '-wd']
-        command_args = ['./MOM_run.csh', '--platform', 'nci', '--type', model_type, '--experiment', exp]
-        run_id = subprocess.check_output(qsub_args + ['--'] + command_args)
+        run_id = subprocess.check_output(['qsub', 'run_script.sh'])
+        run_id = run_id.rstrip()
 
         # Wait for termination.
         self.wait(run_id)
 
-        # Read the output file. 
+        # Read the output file and check that run suceeded.
+        s = ''
+        with open('run_script.x.out', 'r') as f:
+            s = f.read()
+        assert 'run finished' in s
+
+        return s
 
     def wait(self, run_id):
 
@@ -54,8 +85,8 @@ class TestBitReproducibility(ModelTestSetup):
         Run the model for a single day (or month, for now), read a norm from the output, compare to existing norm. 
         """
 
-        expected_norms = [
-                ]
+        expected_norms = []
+
         assert self.build('MOM_SIS')
 
         output = self.run('MOM_SIS', 'om3_core3')
