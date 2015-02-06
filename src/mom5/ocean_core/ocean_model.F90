bc84bc77e4d9ad70a97345a246203dae229d6e95
Yury Vikhliaev <yury.v.vikhliaev@nasa.gov>
2015-02-06 17:08:46 -0500

    bug fix in mom4_put_prog_tracer

--- src/mom5/ocean_core/ocean_model.F90
+++ src/mom5/ocean_core/ocean_model.F90
@@ -2671,7 +2671,7 @@ subroutine mom4_put_prog_tracer(index, fld)
      enddo
   enddo
   call mpp_update_domains(T_prog(index)%field(:,:,:,tau), &
-       Domain%domain2d)
+       Domain%domain2d, complete=T_prog(index)%complete)
 end subroutine mom4_put_prog_tracer
 ! </SUBROUTINE> NAME="mom4_put_prog_tracer"
 
