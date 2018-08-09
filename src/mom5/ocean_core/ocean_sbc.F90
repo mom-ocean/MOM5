dfb28f16176d04b8270b1af28b5a0d3f5878a373
Aidan Heerdegen <aidan.heerdegen@gmail.com>
2018-08-09 16:34:52 +1000

    Fix u_10 initialisation in ocean_sbc (#232)
    10m winds  Velocity%u10 now obtained unconditionally

--- src/mom5/ocean_core/ocean_sbc.F90
+++ src/mom5/ocean_core/ocean_sbc.F90
@@ -3224,10 +3224,8 @@ subroutine get_ocean_sbc(Time, Ice_ocean_boundary, Thickness, Dens, Ext_mode, T_
                 Velocity%vstoke(ii,jj) = Ice_ocean_boundary%vstoke(i,j)
                 Velocity%wavlen(ii,jj)= Ice_ocean_boundary%wavlen(i,j)
              endif
-             Velocity%u10(ii,jj)= Ice_ocean_boundary%wnd(i,j)*Grd%tmask(ii,jj,1) !RASF 10m winds passin ACCESS, Wombat. c.f. MOM6 approach ustar to u10. T-GRID!
           enddo
        enddo
-       call mpp_update_domains(Velocity%u10(:,:)  , Dom%domain2d)
     endif
   !--------momentum fluxes------------------------------------- 
   !
